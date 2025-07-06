2e933db4c6251ce92aacaa4b73a9679ea7f6cde5
danchomas <danchomas@gmail.com>
2025-07-06 14:16:05 +0300

    add try/except

--- src/reader_csv.py
+++ src/reader_csv.py
@@ -1,6 +1,7 @@
 import csv
 import argparse
 import re
+import sys
 from typing import List, Dict, Tuple, Union, Optional
 from tabulate import tabulate
 
@@ -9,52 +10,79 @@ class Reader:
         try:
             with open(path_to_the_file, "r") as file:
                 self.data = list(csv.DictReader(file))
-        except:
-            raise ""
+
+                if not self.data:
+                    print("файл пуст")
+                    sys.exit(0)
+                
+        except FileNotFoundError:
+            print("Файл не был найден")
+            sys.exit(1)
 
     def filter_condition_parser(self, condition: str) -> Tuple[str, str, Union[str, float]]:
+        result = re.split(r"(.+?)([><=]+)(.+)", condition)[1:-1]
+
+        if not result:
+            print("неверный формат для where")
+            sys.exit(1)
+        
+        field, operator, value = result
+            
         try:
-            result = re.split(r"(\w+)([><=]+)(\d+.\d+)", condition)[1:-1] # ['ratings', '==', '4.5'] ['name', '==', 'iphone 12']
-            field, operator, value = result
             value = float(value)
-        except:
-            result = re.split(r"(.+)([><=]+)(.+)", condition)[1:-1]
-            field, operator, value = result
+        except ValueError:
+            pass
+
         return field, operator, value
     
     def _filter(self, condition: str) -> str:
         field, operator, value = self.filter_condition_parser(condition)
         filtered_data = []
+        if field not in self.data[0]:
+            return f"Поле '{field}' не найдено в данных"
         for item in self.data:
             if isinstance(value, float):
                 if operator == ">" and float(item[field]) > value:
                     filtered_data.append(item)
                 elif operator == "<" and float(item[field]) < value:
                     filtered_data.append(item)
-                elif operator == "=" and float(item[field]) == value:
+                elif operator in ["=", "=="] and float(item[field]) == value:
                     filtered_data.append(item)
-            elif operator == "=" and item[field] == value:
+            elif operator in ["=", "=="] and item[field] == value:
                 filtered_data.append(item)
         self.data = filtered_data
         result = tabulate(filtered_data, headers='keys', tablefmt='grid')
         return result
     
     def agregate_parser(self, condition: str) -> Tuple[str, str]:
-        result = re.split(r"(.+)([><=]+)(.+)", condition)[1:-1]
-        field, _, maxminavg = result
-        return field, maxminavg
+        try:
+            field, operation = condition.split("=")
+        except:
+            print("неверный формат для agregate")
+            sys.exit(1)
+        return field, operation
 
     def agregate(self, condition: str) -> str:
-        field, maxminavg = self.agregate_parser(condition)
-        values = [float(item[field]) for item in self.data]
+        field, operation = self.agregate_parser(condition)
+
+        if field not in self.data[0]:
+            print(f"Поле '{field}' не найдено в данных")
+            sys.exit(1)
+        try:
+            values = [float(item[field]) for item in self.data]
+        except ValueError:
+            print(f"Поле '{field}' содержит нечисловые значения")
+            sys.exit(0)
         count = 0
-        if maxminavg == "min":
+        if operation == "min":
             count = min(values)
-        elif maxminavg == "max":
+        elif operation == "max":
             count = max(values)
-        elif maxminavg == "avg":
+        elif operation == "avg":
             count = sum(values) / len(values)
-        data = [dict(field=count)]
+        else:
+            return "такой операции в агрегации пока что не предусмотрено"
+        data = [{operation: count}]
         return tabulate(data, headers='keys', tablefmt='grid')
         
 
